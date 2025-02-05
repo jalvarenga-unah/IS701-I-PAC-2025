2a8261cbb7225d83bd8778e041e8d731851d1698
jalvarenga11 <116615884+jalvarenga11@users.noreply.github.com>
2025-02-04 19:46:34 -0600

    manejo de numpy

---
+++ intro-python/08-numpy.py
@@ -0,0 +1,23 @@
+# numpy
+# para instalar: pip install numpy
+import numpy as np
+
+numpy_list = np.array([1, 2, 3, 4, 5], dtype=float)
+list_zeros = np.zeros(10)
+arange_list = np.arange(10, 15, 2)
+linspace_list = np.linspace(1, 2, 5)
+
+print(numpy_list)
+print(numpy_list * 2)
+print(list_zeros)
+print(arange_list)
+print(linspace_list)
+
+lista_2d = np.array([(1.5, 2, 3), (4, 5, 6)], dtype=float)
+print(lista_2d.shape)
+
+lista_numeros = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+
+# Slicing
+print(lista_numeros.reshape(-1,1)) # la posicion final
+
